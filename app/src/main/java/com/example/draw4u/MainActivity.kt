0d516fb807a9d0aca51553a569510ae92603d1a3
Choi Jongyun <70887404+jongyuni@users.noreply.github.com>
2020-09-27 13:52:13 +0900

    20-09-27

--- app/src/main/java/com/example/draw4u/MainActivity.kt
+++ app/src/main/java/com/example/draw4u/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.draw4u
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this, LoginActivity::class.java))
 
-        setContentView(R.layout.activity_login)
     }
 }
